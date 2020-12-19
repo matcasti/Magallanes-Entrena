6067e974d68ac3ad9506d0c72537f354a03b0ff8
Matías Castillo Aguilar <matiascastilloaguilar@MacBook-Pro-de-Matias.local>
2020-12-19 17:05:35 -0300

    Final tweaks to the report

--- scripts/0.R
+++ scripts/0.R
@@ -75,71 +75,116 @@ data <- within(data, {
   publicidad <- factor(publicidad)
   region <- factor(region)
   sexo <- factor(sexo)
-  sf_1  <- factor(sf_1, levels = c("Mala","Regular","Buena","Muy buena","Excelente"))
-  sf_2  <- factor(sf_2, levels = c("Peor","Algo peor","Igual","Algo mejor","Mucho mejor"))
-  sf_3  <- factor(sf_3, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada"))
-  sf_4  <- factor(sf_4, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada"))
-  sf_5  <- factor(sf_5, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada"))
-  sf_6  <- factor(sf_6, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada"))
-  sf_7  <- factor(sf_7, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada"))
-  sf_8  <- factor(sf_8, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada"))
-  sf_9  <- factor(sf_9, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada"))
-  sf_10 <- factor(sf_10, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada"))
-  sf_11 <- factor(sf_11, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada"))
-  sf_12 <- factor(sf_12, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada"))
+  sf_1  <- factor(sf_1, levels = c("Mala","Regular","Buena","Muy buena","Excelente")) %>% 
+    `levels<-`(c(1,2,3.4,4.4,5)) %>% as.numeric
+  sf_2  <- factor(sf_2, levels = c("Peor","Algo peor","Igual",
+                                   "Algo mejor","Mucho mejor"))
+  sf_3  <- factor(sf_3, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada")) %>% `levels<-`(c(1,2,3)) %>% as.numeric
+  sf_4  <- factor(sf_4, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada")) %>% `levels<-`(c(1,2,3)) %>% as.numeric
+  sf_5  <- factor(sf_5, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada")) %>% `levels<-`(c(1,2,3)) %>% as.numeric
+  sf_6  <- factor(sf_6, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada")) %>% `levels<-`(c(1,2,3)) %>% as.numeric
+  sf_7  <- factor(sf_7, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada")) %>% `levels<-`(c(1,2,3)) %>% as.numeric
+  sf_8  <- factor(sf_8, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada")) %>% `levels<-`(c(1,2,3)) %>% as.numeric
+  sf_9  <- factor(sf_9, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada")) %>% `levels<-`(c(1,2,3)) %>% as.numeric
+  sf_10 <- factor(sf_10, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada")) %>% `levels<-`(c(1,2,3)) %>% as.numeric
+  sf_11 <- factor(sf_11, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada")) %>% `levels<-`(c(1,2,3)) %>% as.numeric
+  sf_12 <- factor(sf_12, levels = c("Sí, muy limitada","Sí, un poco limitada","No, no limitada")) %>% `levels<-`(c(1,2,3)) %>% as.numeric
   sf_13 <- factor(sf_13, levels = c("Nunca","Pocas veces","Algunas veces",
-                                    "La mayor parte del tiempo","Siempre"))
+                                    "La mayor parte del tiempo","Siempre")) %>% 
+    `levels<-`(c(2,1,1,1,1)) %>% as.numeric
   sf_14 <- factor(sf_14, levels = c("Nunca","Pocas veces","Algunas veces",
-                                    "La mayor parte del tiempo","Siempre"))
+                                    "La mayor parte del tiempo","Siempre")) %>% 
+    `levels<-`(c(2,1,1,1,1)) %>% as.numeric
   sf_15 <- factor(sf_15, levels = c("Nunca","Pocas veces","Algunas veces",
-                                    "La mayor parte del tiempo","Siempre"))
+                                    "La mayor parte del tiempo","Siempre")) %>% 
+    `levels<-`(c(2,1,1,1,1)) %>% as.numeric
   sf_16 <- factor(sf_16, levels = c("Nunca","Pocas veces","Algunas veces",
-                                    "La mayor parte del tiempo","Siempre"))
+                                    "La mayor parte del tiempo","Siempre")) %>% 
+    `levels<-`(c(2,1,1,1,1)) %>% as.numeric
   sf_17 <- factor(sf_17, levels = c("Nunca","Pocas veces","Algunas veces",
-                                    "La mayor parte del tiempo","Siempre"))
+                                    "La mayor parte del tiempo","Siempre")) %>% 
+    `levels<-`(c(2,1,1,1,1)) %>% as.numeric
   sf_18 <- factor(sf_18, levels = c("Nunca","Pocas veces","Algunas veces",
-                                    "La mayor parte del tiempo","Siempre"))
+                                    "La mayor parte del tiempo","Siempre")) %>% 
+    `levels<-`(c(2,1,1,1,1)) %>% as.numeric
   sf_19 <- factor(sf_19, levels = c("Nunca","Pocas veces","Algunas veces",
-                                    "La mayor parte del tiempo","Siempre"))
+                                    "La mayor parte del tiempo","Siempre")) %>% 
+    `levels<-`(c(2,1,1,1,1)) %>% as.numeric
   sf_20 <- factor(sf_20, levels = c("De ninguna manera","Un poco","Moderadamente",
-                                    "Bastante","Mucho"))
+                                    "Bastante","Mucho")) %>% 
+    `levels<-`(c(5,4,3,2,1)) %>% as.numeric
   sf_21 <- factor(sf_21, levels = c("Ninguno","Muy poco","Leve","Moderado",
-                                    "Severo","Muy severo"))
+                                    "Severo","Muy severo")) %>% 
+    `levels<-`(c(6,5.4,4.2,3.1,2.2,1)) %>% as.numeric
   sf_22 <- factor(sf_22, levels = c("De ninguna manera","Un poco","Moderadamente",
-                                    "Bastante","Mucho"))
+                                    "Bastante","Mucho")) %>% 
+    `levels<-`(c(6,4.75,3.5,2.25,1)) %>% as.numeric
   sf_23 <- factor(sf_23, levels = c("Nunca","Muy poco tiempo","Un poco",
-                                    "Casi todo el tiempo","Siempre"))
+                                    "Casi todo el tiempo","Siempre")) %>% 
+    `levels<-`(c(1,2.25,3.5,4.75,6)) %>% as.numeric
   sf_24 <- factor(sf_24, levels = c("Nunca","Muy poco tiempo","Un poco",
-                                    "Casi todo el tiempo","Siempre"))
+                                    "Casi todo el tiempo","Siempre")) %>% 
+    `levels<-`(c(6,4.75,3.5,2.25,1)) %>% as.numeric
   sf_25 <- factor(sf_25, levels = c("Nunca","Muy poco tiempo","Un poco",
-                                    "Casi todo el tiempo","Siempre"))
+                                    "Casi todo el tiempo","Siempre")) %>% 
+    `levels<-`(c(6,4.75,3.5,2.25,1)) %>% as.numeric
   sf_26 <- factor(sf_26, levels = c("Nunca","Muy poco tiempo","Un poco",
-                                    "Casi todo el tiempo","Siempre"))
+                                    "Casi todo el tiempo","Siempre")) %>% 
+    `levels<-`(c(1,2.25,3.5,4.75,6)) %>% as.numeric
   sf_27 <- factor(sf_27, levels = c("Nunca","Muy poco tiempo","Un poco",
-                                    "Casi todo el tiempo","Siempre"))
+                                    "Casi todo el tiempo","Siempre"))  %>% 
+    `levels<-`(c(1,2.25,3.5,4.75,6)) %>% as.numeric
   sf_28 <- factor(sf_28, levels = c("Nunca","Muy poco tiempo","Un poco",
-                                    "Casi todo el tiempo","Siempre"))
+                                    "Casi todo el tiempo","Siempre")) %>% 
+    `levels<-`(c(6,4.75,3.5,2.25,1)) %>% as.numeric
   sf_29 <- factor(sf_29, levels = c("Nunca","Muy poco tiempo","Un poco",
-                                    "Casi todo el tiempo","Siempre"))
+                                    "Casi todo el tiempo","Siempre"))  %>% 
+    `levels<-`(c(6,4.75,3.5,2.25,1)) %>% as.numeric
   sf_30 <- factor(sf_30, levels = c("Nunca","Muy poco tiempo","Un poco",
-                                    "Casi todo el tiempo","Siempre"))
+                                    "Casi todo el tiempo","Siempre")) %>% 
+    `levels<-`(c(1,2.25,3.5,4.75,6)) %>% as.numeric
   sf_31 <- factor(sf_31, levels = c("Nunca","Muy poco tiempo","Un poco",
-                                    "Casi todo el tiempo","Siempre"))
+                                    "Casi todo el tiempo","Siempre"))  %>% 
+    `levels<-`(c(6,4.75,3.5,2.25,1)) %>% as.numeric
   sf_32 <- factor(sf_32, levels = c("Nunca","Pocas veces","Algunas veces",
-                                    "La mayor parte del tiempo","Siempre"))
+                                    "La mayor parte del tiempo","Siempre")) %>% 
+    `levels<-`(c(5,4,3,2,1)) %>% as.numeric
   sf_33 <- factor(sf_33, levels = c("Definitivamente falso","Casi siempre falso","No sé",
-                                    "Casi siempre cierto","Definitivamente cierto"))
+                                    "Casi siempre cierto","Definitivamente cierto")) %>% 
+    `levels<-`(c(5,4,3,2,1)) %>% as.numeric
   sf_34 <- factor(sf_34, levels = c("Definitivamente falso","Casi siempre falso","No sé",
-                                    "Casi siempre cierto","Definitivamente cierto"))
+                                    "Casi siempre cierto","Definitivamente cierto")) %>% 
+    `levels<-`(c(1,2,3,4,5)) %>% as.numeric
   sf_35 <- factor(sf_35, levels = c("Definitivamente falso","Casi siempre falso","No sé",
-                                    "Casi siempre cierto","Definitivamente cierto"))
+                                    "Casi siempre cierto","Definitivamente cierto")) %>% 
+    `levels<-`(c(5,4,3,2,1)) %>% as.numeric
   sf_36 <- factor(sf_36, levels = c("Definitivamente falso","Casi siempre falso","No sé",
-                                    "Casi siempre cierto","Definitivamente cierto"))
-  spaq_1 <- as.integer(spaq_1)
+                                    "Casi siempre cierto","Definitivamente cierto")) %>% 
+    `levels<-`(c(1,2,3,4,5)) %>% as.numeric
+  
+  sf.funcion_fisica   <- ((sf_3+sf_4+sf_5+sf_6+sf_7+sf_8+sf_9+sf_10+sf_11+sf_12)-10)/0.2
+  sf.rol_fisico       <- ((sf_13+sf_14+sf_15+sf_16)-4)/0.04
+  sf.dolor_corporal   <- ((sf_21+sf_22)-2)/0.1
+  sf.salud_general    <- ((sf_1+sf_33+sf_34+sf_35+sf_36)-5)/0.2
+  sf.vitalidad        <- ((sf_23+sf_27+sf_29+sf_31)-4)/0.2
+  sf.funcion_social   <- ((sf_20+sf_32)-2)/0.08
+  sf.rol_emocional    <- ((sf_17+sf_18+sf_19)-3)/0.03
+  sf.salud_mental     <- ((sf_24+sf_25+sf_26+sf_28+sf_30)-5)/0.25
+  
+  sf.score <- (sf.funcion_fisica+sf.rol_fisico+sf.dolor_corporal+
+              sf.salud_general+sf.vitalidad+sf.funcion_social+
+              sf.rol_emocional+sf.salud_mental)/8
+  
+  sf.evolución_salud  <- `levels<-`(sf_2,c(5,4,3,2,1)) %>% as.numeric
+  
+  cat_salud.general <- cut(sf.salud_general, breaks = c(0.0,33.3,66.7,100.0), 
+                           labels = c("Mala","Regular","Buena"), include.lowest = T)
+  
+  spaq_1 <- as.numeric(spaq_1)
   spaq_2 <- as.numeric(spaq_2)
-  spaq_3 <- as.integer(spaq_3)
+  spaq_3 <- as.numeric(spaq_3)
   spaq_4 <- as.numeric(spaq_4)
-  spaq_5 <- as.integer(spaq_5)
+  spaq_5 <- as.numeric(spaq_5)
   spaq_6 <- as.numeric(spaq_6)
   spaq_7 <- as.numeric(spaq_7)
   telefono <- factor(telefono)
@@ -164,34 +209,31 @@ with(data,{ tab <- table(comuna)
             return(plot)  })
 
 # Tomamos a las personas de magallanes
+muestra <- data[, 
+  -c("nombre","correo","publicidad","consentimiento")
+  ]
+
+# Tomamos a las personas de magallanes
 magallanes <- data[
   region == levels(region)[7], 
   -c("region","comuna","nombre","correo","publicidad","consentimiento")
   ]
-
-# Imputación de valores perdidos ------------------------------------------
-
-library(mice)           # Ahora imputaremos los valores perdidos usando
-impute <- mice(         # metodo de imputación por ecuaciones encadenadas
-  data = magallanes,    # para lo que usaremos el paquete 'MICE'
-  m = 1, 
-  maxit = 20, 
-  seed = 199318853)
-
-# Ahora reponemos el valor perdido imputado a los datos iniciales de magallanes
-magallanes <- 
-  complete(impute) %>% 
-  as.data.table()
-
 
 
 library(readr)          # Cargamos paquete para guardar los datos limpiados
 write_rds(              # Primero en formato RDS, que será el formato más
   x = magallanes,       # cómodo para trabajar en R.
   file = "data/magallanes.RDS",
-  compress = "none"
-)
+  compress = "none")
 write_csv(              # Luego en formato CSV
   x = magallanes,
-  file = "data/magallanes.csv"
-)
+  file = "data/magallanes.csv")
+write_rds(              # Primero en formato RDS, que será el formato más
+  x = muestra,       # cómodo para trabajar en R.
+  file = "data/muestra.RDS",
+  compress = "none")
+write_csv(              # Luego en formato CSV
+  x = muestra,
+  file = "data/muestra.csv")
+
+rm(data_raw);rm(data)
---
+++ scripts/1 - magallanes.R
@@ -0,0 +1,205 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Proyecto: Taller Magallanes Entrena
+# Organiza: Austral Fitness ®
+# Autor:    Matías Castillo Aguilar
+
+# La finalidad de este código es la de descargar los datos recopilados
+# desde los formularios de inscripción, eliminar a aquellas observaciones
+# que no cumplen con los requisitos y ajustar las variables al formato
+# que usaremos más adelante. 
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Objetivos de análisis ---------------------------------------------------
+
+# 1. Generar análisis descriptivos de tendencia central y dispersión de las
+# variables de actividad física, calidad de vida y variables demográficas.
+#
+# 2. Realizar pruebas correlacionales entre variables de actividad física, 
+# calidad de vida y variables demográficas.
+# 
+# 3. Buscar diferencias significativas entre los niveles de las variables
+# categóricas en las escalas de las variables de actividad física, calidad
+# de vida y variables demográficas.
+# 
+# 4. Hallar asociaciones entre las variables categóricas de actividad física, 
+# calidad de vida y variables demográficas.
+# 
+# 5. Escribir un reporte con los hallazgos de los análisis para ser entregado
+# a los organizadores de la actividad.
+# 
+# 6. Escribir un artículo científico para ser publicado en una revista de
+# carácter científico, luego de su aprobación por un comité de ética científico.
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# 1. Análisis descriptivos ------------------------------------------------
+
+library(data.table);library(tidyverse)
+data <- readRDS(file = "data/magallanes.RDS") %>% 
+  as.data.table() %>% droplevels()
+
+library(gtsummary)
+reset_gtsummary_theme()
+theme_gtsummary_language(
+  language = "es"
+)
+
+table1 <- tbl_summary(data, missing = "no", by = sexo,
+            include = c("edad","sexo","met_total","actividad_fisica","sf.dolor_corporal",
+                    "sf.funcion_fisica","sf.funcion_social","sf.rol_emocional",
+                    "sf.rol_fisico","sf.salud_general","sf.salud_mental","sf.vitalidad",
+                    "sf_2","sf.score"),
+            label = list(edad ~ "Edad",
+                         met_total ~ "MET's /semana",
+                         sf.funcion_fisica ~ "Función física",
+                         sf.rol_fisico ~ "Rol físico",
+                         sf.dolor_corporal ~ "Dolor corporal",
+                         sf.salud_general ~ "Salud general",
+                         sf.vitalidad ~ "Vitalidad",
+                         sf.funcion_social ~ "Función social",
+                         sf.rol_emocional ~ "Rol emocional",
+                         sf.salud_mental ~ "Salud mental",
+                         sf_2 ~ "Evolución de salud",
+                         sf.score ~ "SF-36 Score",
+                         actividad_fisica ~ "Actividad física"),
+          statistic = list(all_continuous() ~ "{mean} ± {sd}",
+                           all_categorical() ~ "{n} ({p}%)"),
+            type = list(sf.dolor_corporal ~ "continuous",
+                        sf.funcion_social ~ "continuous",
+                        sf.rol_emocional ~ "continuous",
+                        sf.rol_fisico ~ "continuous")) %>% 
+    add_p(pvalue_fun = function(x) {style_pvalue(x, digits = 3)},
+          test = list(all_continuous() ~ "t.test")) %>% 
+    add_overall() %>% 
+    modify_footnote(list(p.value ~ "Prueba t-Student; Prueba de asociación chi-cuadrado",
+                        paste("stat_",0:2,sep = "") ~ "Media ± Desviación estándar")) %>% 
+    modify_header(list(label ~ "**Variables**",
+                     stat_0 ~ "**Total**", 
+                     p.value ~ "**P-valor**")) %>%
+    modify_spanning_header(list(paste("stat_",1:2,sep = "") ~ "**Sexo**")) %>% 
+    bold_labels() %>% bold_p() 
+
+
+
+table2 <- tbl_summary(data, missing = "no", by = actividad_fisica,
+            include = c("edad","sexo","met_total","actividad_fisica","sf.dolor_corporal",
+                    "sf.funcion_fisica","sf.funcion_social","sf.rol_emocional",
+                    "sf.rol_fisico","sf.salud_general","sf.salud_mental","sf.vitalidad",
+                    "sf_2","sf.score"),
+            label = list(edad ~ "Edad",
+                         met_total ~ "MET's /semana",
+                         sf.funcion_fisica ~ "Función física",
+                         sf.rol_fisico ~ "Rol físico",
+                         sf.dolor_corporal ~ "Dolor corporal",
+                         sf.salud_general ~ "Salud general",
+                         sf.vitalidad ~ "Vitalidad",
+                         sf.funcion_social ~ "Función social",
+                         sf.rol_emocional ~ "Rol emocional",
+                         sf.salud_mental ~ "Salud mental",
+                         sf_2 ~ "Evolución de salud",
+                         sf.score ~ "SF-36 Score",
+                         sexo ~ "Sexo"),
+            statistic = list(all_continuous() ~ "{mean} ± {sd}",
+                             all_categorical() ~ "{n}/{N} ({p}%)"),
+            type = list(sf.dolor_corporal ~ "continuous",
+                        sf.funcion_social ~ "continuous",
+                        sf.rol_emocional ~ "continuous",
+                        sf.rol_fisico ~ "continuous")) %>% 
+  add_p(pvalue_fun = function(x) {style_pvalue(x, digits = 3)},
+        test = list(all_continuous() ~ "aov")) %>% 
+  modify_footnote(list(p.value ~ "Análisis de varianza (ANOVA) de una vía; Prueba de asociación chi-cuadrado",
+                       paste("stat_",1:3,sep = "") ~ "Media ± Desviación estándar")) %>% 
+  modify_header(list(label ~ "**Variables**", p.value ~ "**P-valor**")) %>%
+  modify_spanning_header(list(paste("stat_",1:3,sep = "") ~ "**Actividad física**")) %>% 
+  bold_labels() %>% bold_p() 
+
+# 2. Diferencias de grupos ------------------------------------------------
+
+library(ggstatsplot)
+plot1 <- ggplot(data, aes(x = sexo, y = sf.dolor_corporal, fill = sexo)) +
+  labs(x = "", y = "Función frente al dolor\n") +
+  stat_summary(geom = "bar", fun = "mean", alpha = 0.75) +
+  stat_summary(geom = "errorbar", fun.data = "mean_ci", width = 0.3) +
+  theme_pubr()
+plot1 <- set_palette(plot1, "grey") + theme(legend.position = "none")
+
+plot2 <- ggplot(data, aes(x = sexo, y = sf.funcion_social, fill = sexo)) +
+  labs(x = "", y = "Función social\n") +
+  stat_summary(geom = "bar", fun = "mean", alpha = 0.75) +
+  stat_summary(geom = "errorbar", fun.data = "mean_ci", width = 0.3) +
+  theme_pubr()
+plot2 <- set_palette(plot2, "grey") + theme(legend.position = "none")
+
+plot3 <- ggplot(data, aes(x = sexo, y = sf.salud_general, fill = sexo)) +
+  labs(x = "", y = "Salud general\n") +
+  stat_summary(geom = "bar", fun = "mean", alpha = 0.75) +
+  stat_summary(geom = "errorbar", fun.data = "mean_ci", width = 0.3) +
+  theme_pubr()
+plot3 <- set_palette(plot3, "grey") + theme(legend.position = "none")
+
+plot4 <- ggplot(data, aes(x = sexo, y = sf.score, fill = sexo)) +
+  labs(x = "", y = "Promedio SF-36 por área\n") +
+  stat_summary(geom = "bar", fun = "mean", alpha = 0.75) +
+  stat_summary(geom = "errorbar", fun.data = "mean_ci", width = 0.3) +
+  theme_pubr()
+plot4 <- set_palette(plot4, "grey") + theme(legend.position = "none")
+
+library(ggpubr)
+plot.sexo <- ggarrange(
+  plot1, plot2, plot3, plot4, 
+  labels = c("A.","B.","C.","D."),
+  align = "hv"
+)
+
+plot5 <- ggplot(data, aes(x = actividad_fisica, y = met_total, 
+                          fill = actividad_fisica)) +
+  labs(x = "", y = "MET's /semana\n") +
+  stat_summary(geom = "bar", fun = "mean", alpha = 0.75) +
+  stat_summary(geom = "errorbar", fun.data = "mean_ci", width = 0.3) +
+  ggsignif::geom_signif(comparisons = list(
+    c("Bajo","Moderado"),c("Moderado","Alto"),c("Bajo","Alto")),
+    test = "t.test", test.args = list(var.equal = TRUE), step_increase = 1/20, 
+    map_signif_level = function(p)sprintf("p = %.2g", p), 
+    margin_top = -1/1.75) +
+  theme_pubr()
+plot5 <- set_palette(plot5, "grey") + theme(legend.position = "none")
+
+plot6 <- ggplot(data, aes(x = actividad_fisica, y = sf.salud_general, 
+                          fill = actividad_fisica)) +
+  labs(x = "", y = "Salud general\n") +
+  stat_summary(geom = "bar", fun = "mean", alpha = 0.75) +
+  stat_summary(geom = "errorbar", fun.data = "mean_ci", width = 0.3) +
+  ggsignif::geom_signif(comparisons = list(
+    c("Bajo","Moderado"),c("Moderado","Alto"),c("Bajo","Alto")),
+    test = "t.test", test.args = list(var.equal = TRUE), step_increase = 1/18, 
+    map_signif_level = function(p)sprintf("p = %.2g", p), 
+    margin_top = -1/2.5) +
+  theme_pubr()
+plot6 <- set_palette(plot6, "grey") + theme(legend.position = "none")
+
+plot7 <- ggplot(data, aes(x = actividad_fisica, y = sf.rol_emocional, 
+                          fill = actividad_fisica)) +
+  labs(x = "", y = "Rol emocional\n") +
+  stat_summary(geom = "bar", fun = "mean", alpha = 0.75) +
+  stat_summary(geom = "errorbar", fun.data = "mean_ci", width = 0.3) +
+  ggsignif::geom_signif(comparisons = list(
+    c("Bajo","Moderado"),c("Moderado","Alto"),c("Bajo","Alto")),
+    test = "t.test", test.args = list(var.equal = TRUE), step_increase = 1/14, 
+    map_signif_level = function(p)sprintf("p = %.2g", p), 
+    margin_top = 0) +
+  theme_pubr()
+plot7 <- set_palette(plot7, "grey") + theme(legend.position = "none")
+
+plot.acfisica <- ggarrange(
+  plot7, 
+  ggarrange(plot5, plot6, labels = c("B.","C."),
+            align = "hv"),
+  nrow = 2,labels = "A."
+)
+
+save(list = ls(), file = "output/output-magallanes.RData")
+
+
+
